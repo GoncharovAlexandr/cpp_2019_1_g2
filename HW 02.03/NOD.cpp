250801d07eed9d4dda7780708ae03260fb025b45
GoncharovAlexandr <48448480+GoncharovAlexandr@users.noreply.github.com>
2019-03-12 16:00:43 +0300

    Nod_Goncharov.cpp

--- HW 02.03/NOD.cpp
+++ HW 02.03/NOD.cpp
@@ -21,4 +21,28 @@ A >= 0 ; B >= 0;
 
 */
 
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+float a, b, c, x1, x2, d;
+cin >> a >> b >>c;
+if ( a == 0 ){
+x1 = -c/b;
+cout << x1;
+} else {
+d = b*b-4*a*c;
+if ( a != 0 && d > 0 ){
+x1 = (-b + sqrt(d))/(2*a);
+x2 = (-b - sqrt(d))/(2*a);
+cout << x1 << " " << x2;
+}
+if ( a != 0 && d == 0 ){
+x1 = (-b)/(2*a);
+cout << x1;
+}
+}
+return 0;
+}
+
 
